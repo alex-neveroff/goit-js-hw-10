33a41689520e08b31c3a638d8ffd3844fdcf44c9
Alexandr Neveroff <neveroff1976@gmail.com>
2023-03-21 23:32:21 +0200

    function for api request

---
+++ src/js/api-service.js
@@ -0,0 +1,13 @@
+const BASE_URL = 'https://restcountries.com/v3.1/';
+const options = 'fields=name,capital,population,flags,languages';
+
+export default function fetchCountries(name) {
+  return fetch(`${BASE_URL}/name/${name.toLowerCase()}?${options}`).then(
+    response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    }
+  );
+}
